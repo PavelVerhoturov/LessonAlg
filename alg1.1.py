5bb1347688ec5b448fda1cc08486df705fb7c9a4
PavelVerhoturov <tolstiak1991@mail.ru>
2021-07-20 18:42:42 +0700

    Completed task 1

---
+++ alg1.1.py
@@ -0,0 +1,15 @@
+x = input('Введите трехзначное число ')
+a = x[0]
+b = x[1]
+c = x[2]
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+sum = a + b + c
+mult = a*b*c
+print(f'Сумма чисел = {sum}')
+print(f'Произведение чисел = {mult}')
+
+
