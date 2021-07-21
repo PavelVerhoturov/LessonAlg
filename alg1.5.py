cb3b88bc53b1482018437b0209a381fe0fa19f5d
PavelVerhoturov <tolstiak1991@mail.ru>
2021-07-21 12:10:49 +0700

    start = ord('a') - 1

--- alg1.5.py
+++ alg1.5.py
@@ -5,6 +5,6 @@ start = ord('a') - 1
 
 side_a = ord(a.lower()) - start
 side_b = ord(b.lower()) - start
-range_ab = side_b - side_a
+range_ab = side_b - side_a - 1
 print(f'В алфавите {a} имеет позицию {side_a}, {b} позицию {side_b}.')
 print(f'Колличество букв между ними {range_ab}')
